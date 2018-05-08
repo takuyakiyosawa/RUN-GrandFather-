dd6d1fdbbabf8ef997a3de2e03aaca71fb8434f9
DESKTOP-LUBM5SJ\g1823028 <takuya.kiyosawa0920@gmail.com>
2018-05-08 21:33:38 +0900

    正式版　PUSH

---
+++ Assets/Maincamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Maincamera : MonoBehaviour {
+	public GameObject player;
+	public GameObject mainCamera;
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mainCamera.transform.position = new Vector3(4.8f,2.1f,player.transform.position.z);
+
+		}
+}
